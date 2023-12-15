7e0c4696fdec3f14b56932b17560bdba312673d4
Volodymyr Vienko <127095667+vova4211@users.noreply.github.com>
2023-12-15 13:52:15 +0200

    Add files via upload

---
+++ HomeWork4/Person.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4
+{
+    internal class Person
+    {
+        private string name;
+        private DateTime birtYear;
+
+        public string Name
+        {
+            get { return name; }
+            
+        }
+
+        public DateTime BirtYear
+        {
+            get { return birtYear; }
+           
+        }
+
+        public Person() { }
+
+        public Person(string name, DateTime birtYear)
+        {
+            this.name = name;
+            this.birtYear = birtYear;
+        }
+
+        public int Age() 
+        {
+         
+            int age = DateTime.Now.Year - birtYear.Year;
+            return age;
+        }
+
+        public static Person Input(int i)
+        {
+            Console.WriteLine($"Enter name {i + 1}th person");
+            string name = Console.ReadLine();
+            Console.WriteLine($"enter  year of birth {i + 1}th person" );
+            DateTime birtYear = new DateTime(Convert.ToInt16(Console.ReadLine()),1,1);
+            Person person = new Person(name, birtYear);
+            return person;
+        }
+
+        public void ChangeName(string newName)
+        {
+            name = newName;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        public override string ToString()
+        {
+            return ($"{name} was borned in {birtYear.Year}");
+        }
+
+        public static bool operator == ( Person a, Person b )
+        {
+            return (a.name == b.name);
+        }
+        public static bool operator != ( Person a, Person b)
+        {
+            return !(a.name == b.name);
+        }
+    }
+}
---
+++ HomeWork4/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace HomeWork4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Person person = new Person();
+            person.Output();
+
+            Person[] people = new Person[6];
+
+            for(int i = 0; i < people.Length; i++)
+            {
+                people[i] = Person.Input(i);
+            }
+
+            for (int i = 0;i < people.Length; i++)
+            {
+                people[i].Output();
+            }
+
+            for (int i = 0; i < people.Length; i++)
+            {
+
+                Console.WriteLine($"Name: {people[i].Name}, Age: {people[i].Age()}");
+            }
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (people[i].Age() < 16)
+                {
+                    people[i].ChangeName("Very Young");
+                }
+            }
+
+            for(int i = 0; i < people.Length; i++)
+            {
+                people[i].Output();
+            }
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                for (int j = i + 1; j < people.Length; j++)
+                {
+                    if (people[i] == people[j])
+                    {
+                        Console.WriteLine($"{people[i]} and {people[j]}  same name");
+                    }
+                   
+                }
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
